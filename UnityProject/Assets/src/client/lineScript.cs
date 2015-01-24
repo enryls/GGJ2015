13f01ec905690e73b8c5bc50c8f15030e4ab5e9f
Kons <kons0@libero.it>
2015-01-24 18:19:33 +0100

    Figa gli add

---
+++ UnityProject/Assets/src/client/lineScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class lineScript : MonoBehaviour {
+
+	void OnNetworkInstantiate(NetworkMessageInfo info) {
+		Debug.Log("LS: " + networkView.viewID + " spawned");
+
+		authPlayer.lineDict.Add(networkView.viewID, transform);
+	}
+
+	[RPC]
+	public void updateLine(NetworkViewID instance, Vector3[] tempStored) {
+		//Debug.Log("updateLine " + instance);
+		if (!authPlayer.lol.ContainsKey(instance))
+			authPlayer.lol.Add(instance, null);
+		authPlayer.lol[instance] = new List<Vector3>(tempStored);
+	}
+}
