cc9be40b80f0ee09429467baa2abe4f721807748
Enrico <enryls@hotmail.it>
2015-01-25 15:59:30 +0100

    - Aggiunta Musica e Immagini;

--- UnityProject/Assets/Scripts/Menu.cs
+++ UnityProject/Assets/Scripts/Menu.cs
@@ -2,18 +2,46 @@
 using System.Collections;
 
 public class Menu : MonoBehaviour {
-	public bool isStart = false;
+	public bool isStart1 = false;
+	public bool isStart2 = false;
 	// Use this for initialization
-	void Start () {
+	Color onMouseEntercolor1;
+	Color onMouseEntercolor2;
+	Color maincolor1;
+	Color maincolor2;
 	
+	void Start () {
+		maincolor1 = new Color (0.2509f, 0.0862f, 0.0392f, 1f);
+		maincolor2 = new Color (0.4509f, 0.32941f, 0.2039f, 1f);
+		onMouseEntercolor1 = new Color (0.7490f, 0.6666f, 0.4509f, 1f);
+		onMouseEntercolor2 = new Color (0.5490f, 0.4509f, 0.2941f, 1f);
+		if(isStart1)
+			renderer.material.color = maincolor1;
+		else if(isStart2)
+			renderer.material.color = maincolor2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
+	void OnMouseEnter(){
+		if(isStart1)
+			renderer.material.color = onMouseEntercolor1;
+		else if(isStart2)
+			renderer.material.color = onMouseEntercolor2;
+	}
 	
+	void OnMouseExit(){
+		if(isStart1)
+			renderer.material.color = maincolor1;
+		else if(isStart2)
+			renderer.material.color = maincolor2;
+		
 	}
 	void OnMouseUp(){
-		if (isStart)
+		if (isStart1) {
 			Application.LoadLevel ("Connessione");
+		}
 	}
 }
--- UnityProject/Assets/src/mainScreen.cs
+++ UnityProject/Assets/src/mainScreen.cs
@@ -2,27 +2,27 @@
 using System.Collections;
 
 public class mainScreen : MonoBehaviour {
-
+	
 	public string connectionIP = "192.168.43.43";
 	public int connectionPort = 25001;
-
+	
 	// Use this for initialization
 	void Start() {
 		Application.runInBackground = true ;
 	}
-
+	
 	void OnGUI() {
 		if (Network.peerType == NetworkPeerType.Disconnected) {
-			GUI.Label(new Rect(10, 10, 300, 20), "Status: Disconnected");
-			if (GUI.Button(new Rect(10, 30, 120, 20), "Client Connect")) {
+			GUI.Label(new Rect(500, 250, 300, 100), "Status: Disconnected");
+			if (GUI.Button(new Rect(450, 300, 220, 100), "Client Connect")) {
 				Network.Connect(connectionIP, connectionPort);
 				Application.LoadLevel("networkScene");
 			}
-			if (GUI.Button(new Rect(10, 50, 120, 20), "Initialize Server")) {
+			if (GUI.Button(new Rect(450, 400, 220, 100), "Initialize Server")) {
 				Network.InitializeServer(32, connectionPort, false);
 				Application.LoadLevel("networkScene");
 			}
-			if (GUI.Button(new Rect(10, 70, 120, 20), "Reset")) {
+			if (GUI.Button(new Rect(450, 500, 220, 100), "Reset")) {
 				var objects = GameObject.FindObjectsOfType<GameObject>();
 				foreach (GameObject o in objects) {
 					if (o.name == "floor")
@@ -33,7 +33,7 @@ public class mainScreen : MonoBehaviour {
 						Destroy(o.gameObject);
 					if (o.GetComponent<BoxCollider2D>())
 						Destroy(o.gameObject);
-
+					
 					//Continue resetting things
 					sv_spawn svs = o.GetComponent<sv_spawn>();
 					if (svs) {
@@ -50,13 +50,13 @@ public class mainScreen : MonoBehaviour {
 				}
 			}
 		} else if (Network.peerType == NetworkPeerType.Client) {
-			GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Client");
-			if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect")) {
+			GUI.Label(new Rect(500, 10, 300, 100), "Status: Connected as Client");
+			if (GUI.Button(new Rect(500, 300, 220, 100), "Disconnect")) {
 				Network.Disconnect(200);
 			}
 		} else if (Network.peerType == NetworkPeerType.Server) {
-			GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Server");
-			if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect")) {
+			GUI.Label(new Rect(500, 10, 300, 100), "Status: Connected as Server");
+			if (GUI.Button(new Rect(500, 300, 220, 100), "Disconnect")) {
 				Network.Disconnect(200);
 			}
 		}
